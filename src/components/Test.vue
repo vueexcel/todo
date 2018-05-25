6286812a30d62fdcb48e25312324301528c61113
crypto <crypto>
2018-05-25 15:23:21 +0530

    click events

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -3,8 +3,12 @@
         {{name}} Testing
         <br/>
         <br/>
-        
+
         <input type="text" v-model="name" />
+
+        <button v-on:click="clear">Clear</button>
+
+        <button @click="makeCapital">Caps</button>
     </div>
 </template>
 
@@ -12,10 +16,18 @@
 <script>
 export default {
   name: "Test",
-  data : () => {
-      return {
-          name: "manish1"
-      }
+  data: () => {
+    return {
+      name: "manish1"
+    };
+  },
+  methods: {
+    clear: function() {
+      this.name = "";
+    },
+    makeCapital: function(){
+        this.name = this.name.toUpperCase();
+    }
   }
 };
 </script>
