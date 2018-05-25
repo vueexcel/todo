1de5e11c062846be6ffdd0cf548ccbd482b1570d
crypto <crypto>
2018-05-25 18:52:34 +0530

    array editing

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -9,7 +9,12 @@
         </form>
 
         <ul>
-            <li v-for="(item, index) in data" v-bind:key="index">{{item}}</li>
+            <li v-for="(item, index) in data" v-bind:key="index">
+                <span>{{item}}</span>
+
+                <a href="#" v-on:click.prevent="deleteItem(index)" >Delete</a>
+                <a href="#" v-on:click.prevent="editItem(index)" >Edit</a>
+            </li>
         </ul>
 
     </div> 
@@ -17,28 +22,51 @@
 
 
 <script>
+import Vue from 'vue'
+
 export default {
   name: "Test",
   data: function() {
     return {
       name: "",
-      data: []
+      data: [],
+      editIndex: false
     };
   },
   methods: {
     submit: function(e) {
       if (e.keyCode === 13) {
-        this.data.push(this.name);
-        this.name = "";
+        if (this.editIndex) {
+            // this.data.splice(this.editIndex,1,this.name);
+            //split also works fine, but just trying out Vue.set
+            Vue.set(this.data, this.editIndex, this.name);
+            this.name = "";
+            this.editIndex = -1;
+        } else {
+          this.data.push(this.name);
+          this.name = "";
+        }
       }
+    },
+    deleteItem: function(index) {
+      this.data.splice(index, 1);
+    },
+    editItem: function(index) {
+      this.editIndex = index;
+      this.name = this.data[index];
     }
   }
 };
 </script>
 <style>
-#testing li{
-    border: 1px solid;
-    padding: 5px;
+#testing li {
+  border: 1px solid;
+  padding: 5px;
+}
+#testing li a {
+  font-size: 10px;
+  float: right;
+  padding: 5px;
 }
 </style>
 
