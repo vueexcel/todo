833880d2a416ed65422dfb4225e0a02c32aeb467
crypto <crypto>
2018-05-25 15:02:29 +0530

    added test component

---
+++ src/components/Test.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="testing">
+        {{name}} Testing
+    </div>
+</template>
+
+
+<script>
+export default {
+  name: "Test",
+  data : () => {
+      return {
+          name: "manish1"
+      }
+  }
+};
+</script>
+<style>
+</style>
+
