612f0e5654a741023c849e0ae789a697a5dbf35b
crypto <crypto>
2018-06-01 15:04:41 +0530

    add user form

--- src/components/AddUser.vue
+++ src/components/AddUser.vue
@@ -1,16 +1,36 @@
 <template>
     <div class="row top-row">
         <div class="col-md-3 offset-md-9">
-            <button type="button" @click="openPopup" class="button">Add User</button>
+            <button type="button" @click="isComponentModalActive = true" class="button">Add User</button>
         </div>
+
+        <b-modal :active.sync="isComponentModalActive" has-modal-card>
+            <add-user-form v-bind="formProps" @adduser="adduser"></add-user-form>
+        </b-modal>
     </div>
 </template>
 
 <script>
+import AddUserForm from "./AddUserForm";
 export default {
   name: "AddUser",
+  components: { AddUserForm },
   methods: {
-    openPopup: function() {}
+    adduser: function(user) {
+      // eslint-disable-next-line
+      console.log(user);
+    }
+  },
+  data() {
+    return {
+      isComponentModalActive: false,
+      formProps: {
+        email: "",
+        password: "",
+        name: "",
+        date: null
+      }
+    };
   }
 };
 </script>
---
+++ src/components/AddUserForm.vue
@@ -0,0 +1,119 @@
+<template>
+    <form @submit.prevent="submitForm">
+                <div class="modal-card" style="width: auto">
+                    <header class="modal-card-head">
+                        <p class="modal-card-title">Login</p>
+                    </header>
+                    <section class="modal-card-body">
+                        <b-field label="Email">
+                            <b-input
+                                type="email"
+                                v-model="form_email"
+                                placeholder="Your email"
+                                required
+                                >
+                            </b-input>
+                        </b-field>
+
+                        <b-field label="Name">
+                            <b-input
+                                type="name"
+                                v-model="form_name"
+                                placeholder="Your name"
+                                required
+                                >
+                            </b-input>
+                        </b-field>
+
+                        <b-field label="Date of birth">
+                            <b-datepicker
+                                required
+                                v-model="dob"
+                                placeholder="dob.."
+                                icon="calendar-today">
+                            </b-datepicker>
+                        </b-field>
+
+                        <b-field label="Password">
+                            <b-input
+                                required
+                                type="password"
+                                v-model="form_password"
+                                placeholder="Your password"
+                                >
+                            </b-input>
+                        </b-field>
+
+                        <b-field label="Confirm Password" 
+                            :type="password_mismatch_message_class" 
+                            :message="password_mismatch_message">
+                            <b-input
+                                required
+                                type="password"
+                                v-model="conf_password"
+                                password-reveal
+                                placeholder="Confirm password"
+                                >
+                            </b-input>
+                        </b-field>
+                        <b-field :type="terms_message_class" 
+                            :message="terms_message">
+                            <b-checkbox required v-model="terms" >Accept Terms & Conditions</b-checkbox>
+                        </b-field>
+                    </section>
+                    <footer class="modal-card-foot">
+                        <button class="button" type="button" @click="$parent.close()">Close</button>
+                        <button type="submit" class="button is-primary">Login</button>
+                    </footer>
+                </div>
+            </form>
+</template>
+
+<script>
+export default {
+  name: "AddUserForm",
+  props: ["email", "name", "password", "date"],
+  data: function() {
+    return {
+      form_email: "",
+      form_name: "",
+      form_password: "",
+      terms: false,
+      conf_password: "",
+      password_mismatch_message: "",
+      password_mismatch_message_class: "",
+      terms_message: "",
+      terms_message_class: "",
+      dob: null
+    };
+  },
+  methods: {
+    submitForm: function() {
+      if (this.form_password !== this.conf_password) {
+        this.password_mismatch_message = "Both passwords should match";
+        this.password_mismatch_message_class = "is-danger";
+      } else {
+        this.password_mismatch_message = "";
+        this.password_mismatch_message_class = "";
+      }
+      if (!this.terms) {
+        this.terms_message = "Please accept terms";
+        this.terms_message_class = "is-danger";
+      } else {
+        this.terms_message = "";
+        this.terms_message_class = "";
+      }
+      this.$emit('adduser',{
+        email: this.form_email,
+        name: this.form_name,
+        password: this.form_password,
+        data: this.dob
+      });
+      this.$parent.close();
+    }
+  }
+};
+</script>
+
+<style>
+</style>
