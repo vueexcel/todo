9a7de01a85d3102b6941ebc13a1ba46a26883c80
crypto <crypto>
2018-06-03 14:11:06 +0530

    basic setup

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
+import Buefy from 'buefy'
+import 'buefy/lib/buefy.css'
+
+Vue.use(Buefy)
+Vue.use(Vuex)
+
 
 Vue.config.productionTip = false
 
