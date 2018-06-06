670788850bb02741f39fd723cd6bdaf822f30cad
crypto <crypto>
2018-06-06 22:32:56 +0530

    login persisted and automatically redirect to profile page

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,20 @@
 
 <script>
 import Home from "./pages/Home.vue";
+import { mapGetters } from "vuex";
 
 export default {
   name: "app",
   components: {
     Home
+  },
+  computed: {
+    ...mapGetters(["isLoggedIn"])
+  },
+  mounted: function() {
+    if (this.isLoggedIn) {
+      this.$router.push("/profile");
+    }
   }
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import login from './modules/login'
+import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex)
 
@@ -10,5 +11,6 @@ export default new Vuex.Store({
   modules: {
     login,
   },
-  strict: debug
+  strict: debug,
+  plugins: [createPersistedState()]
 })
